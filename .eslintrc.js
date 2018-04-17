1e4ed2708576f1a736265f14d215afd83a63f067
Nathaniel Green <11984722+nathanielgreen@users.noreply.github.com>
2018-04-17 21:07:07 +0100

    [BU-2] (Task) SCSS & AirBnB ESLint (#1)
    * BU-2: Added scss
    
    * BU-2: Added ESLinting and fixed errors
    
    * BU-2: Fixed imports throwing eslint error

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,5 @@
+const resolve = require('path').resolve;
+
 module.exports = {
   root: true,
   env: {
@@ -11,11 +13,36 @@ module.exports = {
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
     'plugin:vue/essential',
+    'airbnb-base',
   ],
   // required to lint *.vue files
   plugins: [
+    'import',
     'vue'
   ],
   // add your custom rules here
-  rules: {}
+  rules: {
+  },
+  settings: {
+    'import/resolver': {
+      webpack: {
+        config: {
+          resolve: {
+            alias: {
+              '~': __dirname,
+              'static': resolve(__dirname, 'static'), // use in template with <img src="~static/nuxt.png" />
+              '~static': resolve(__dirname, 'static'),
+              'assets': resolve(__dirname, 'assets'), // use in template with <img src="~static/nuxt.png" />
+              '~assets': resolve(__dirname, 'assets'),
+              '~plugins': resolve(__dirname, 'plugins'),
+              '~store': resolve(__dirname, '.nuxt/store'),
+              '~router': resolve(__dirname, '.nuxt/router'),
+              '~pages': resolve(__dirname, 'pages'),
+              '~components': resolve(__dirname, 'components')
+            }
+          }
+        }
+      }
+    }
+  }
 }
