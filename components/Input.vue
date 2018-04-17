f6212489365a0df674a151169003f329ab90dca6
Nathaniel Green <ngreen2303@gmail.com>
2018-04-17 21:32:26 +0100

    BU-1: Created Note Input

---
+++ components/Input.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="input">
+    <form v-on:submit="addNote($event)">
+    <input
+      class="input__input"
+      type="text"
+      placeholder="Type here..."
+    />
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Input',
+  methods: {
+    addNote(event) {
+      event.preventDefault();
+      console.log('hi');
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.input {
+  &__input {
+    font-size:18px;
+    padding:10px 10px 10px 5px;
+    display:block;
+    width:300px;
+    border:none;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,15 @@
 <template>
   <section class="container">
-    <div>
-      <logo/>
-      <h1 class="title">
-        bulletjournal
-      </h1>
-      <h2 class="subtitle">
-        My magnificent Nuxt.js project
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
-      </div>
-    </div>
+    <Input/>
   </section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue';
+import Input from '~/components/Input.vue';
 
 export default {
   components: {
-    Logo,
+    Input,
   },
 };
 </script>
