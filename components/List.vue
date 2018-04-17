dc656455c26adbb3fb4f134c6d5b646414995e20
Nathaniel Green <ngreen2303@gmail.com>
2018-04-17 21:47:12 +0100

    BU-1: Added List component

---
+++ components/List.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="list">
+    <ul class="list__list" v-for="(value, index) in items" :key="index">
+      <li class="list__list-item">{{ value }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  name: 'List',
+  computed: {
+    ...mapGetters({
+      items: 'getItems',
+    }),
+  },
+};
+</script>
+
+<style lang="scss">
+</style>
