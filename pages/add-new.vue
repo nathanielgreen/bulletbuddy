9714ea7f59cd016a7e1d8ed3a6cf058218eccf5c
Nathaniel Green <ngreen2303@gmail.com>
2018-04-17 23:31:57 +0100

    BU-4: Created add new page

---
+++ pages/add-new.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="add-new">
+    <button
+      class="add-new__button"
+    >New Daily Log</button>
+  </div>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style lang="scss">
+</style>
