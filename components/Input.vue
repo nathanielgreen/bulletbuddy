8a408cd2db878d777c7581bbbe23b3f66152d971
Nathaniel Green <11984722+nathanielgreen@users.noreply.github.com>
2018-04-17 22:23:58 +0100

    [BU-1] (Feature) Implement Notes (#2)
    * BU-1: Created Note Input
    
    * BU-1: Adding Vuex methods
    
    * BU-1: Added List component
    
    * BU-1: Items now added to Vuex store and appear

---
+++ components/Input.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="input">
+    <form v-on:submit="addNote($event)">
+    <input
+      class="input__input"
+      v-model="item"
+      type="text"
+      placeholder="Type here..."
+    />
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Input',
+  data() {
+    return {
+      item: '',
+    };
+  },
+  methods: {
+    addNote(event) {
+      event.preventDefault();
+      this.$store.commit('ADD_ITEM', this.item);
+      this.item = '';
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.input {
+  &__input {
+    font-size:18px;
+    padding:10px 10px 10px 5px;
+    display:block;
+    width:300px;
+    border:none;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,18 @@
 <template>
   <section class="container">
-    <div>
-      <logo/>
-      <h1 class="title">
-        bulletjournal
-      </h1>
-      <h2 class="subtitle">
-        My magnificent Nuxt.js project
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
-      </div>
-    </div>
+    <List></List>
+    <Input />
   </section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue';
+import List from '~/components/List.vue';
+import Input from '~/components/Input.vue';
 
 export default {
   components: {
-    Logo,
+    List,
+    Input,
   },
 };
 </script>
