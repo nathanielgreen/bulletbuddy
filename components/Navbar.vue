d39f5cb5cc75cac1244fe9ab2022bf3396342ae5
Nathaniel Green <11984722+nathanielgreen@users.noreply.github.com>
2018-04-19 00:34:56 +0100

    [BU-9] (Feature) Create Navbar (#8)
    * BU-9: Added basic navbar setup
    
    * BU-9: Cleaning up list

---
+++ components/Navbar.vue
@@ -0,0 +1,27 @@
+<template>
+  <nav class="navbar">
+    <nuxt-link class="navbar__item" to="/">Index</nuxt-link>
+    <nuxt-link class="navbar__item" to="/add-new">New Log</nuxt-link>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+};
+</script>
+
+<style lang="scss">
+.navbar {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  &__item {
+    flex: 1;
+    height: 100%;
+    text-align: center;
+    margin: 0 auto;
+  }
+}
+</style>
