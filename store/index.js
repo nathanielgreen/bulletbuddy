2700efa9d50e897034dd912990e2c414111e987f
Nathaniel Green <ngreen2303@gmail.com>
2018-04-23 18:58:13 +0100

    BU-27: Split out mutations

--- store/index.js
+++ store/index.js
@@ -1,6 +1,6 @@
-import Vue from 'vue';
 import Vuex from 'vuex';
 import actions from './actions';
+import mutations from './mutations';
 
 const store = () => new Vuex.Store({
   state: {
@@ -18,72 +18,7 @@ const store = () => new Vuex.Store({
     getPages: state => state.pages,
     getModal: state => state.modal,
   },
-  mutations: {
-    ADD_NOTE(state, val) {
-      const size = Object.keys(state.viewedItems).length;
-      const note = {
-        index: size + 1,
-        type: 'note',
-        value: val,
-      };
-      Vue.set(state.viewedItems, (size + 1), note);
-    },
-    ADD_EVENT(state, val) {
-      const size = Object.keys(state.viewedItems).length;
-      const event = {
-        index: size + 1,
-        type: 'event',
-        value: val,
-      };
-      Vue.set(state.viewedItems, (size + 1), event);
-    },
-    ADD_TASK(state, val) {
-      const size = Object.keys(state.viewedItems).length;
-      const task = {
-        index: size + 1,
-        type: 'task',
-        value: val,
-        checked: false,
-      };
-      Vue.set(state.viewedItems, (size + 1), task);
-    },
-    DELETE_ITEM(state) {
-      Vue.delete(state.viewedItems, state.modal.item.index);
-    },
-    DELETE_PAGE(state, index) {
-      state.pages.splice(index, 1);
-    },
-    TOGGLE_TASK(state, data) {
-      const task = state.viewedItems[data.index];
-      task.checked = !task.checked;
-    },
-    TOGGLE_SHOW_MODAL(state, value) {
-      state.modal.show = !state.modal.show;
-      state.modal.item = value;
-    },
-    ADD_NEW_DAILY_LOG(state, currentDate) {
-      const page = {
-        type: 'DL',
-        header: currentDate,
-        items: {},
-      };
-      state.pages.push(page);
-    },
-    ADD_NEW_MONTHLY_LOG(state, currentMonth) {
-      const page = {
-        type: 'ML',
-        header: currentMonth,
-        items: {},
-      };
-      state.pages.push(page);
-    },
-    UPDATE_VIEWED_ITEMS(state, value) {
-      state.viewedItems = state.pages[value].items;
-    },
-    UPDATE_VIEWED_HEADER(state, value) {
-      state.viewedHeader = state.pages[value].header;
-    },
-  },
+  mutations,
   actions,
 });
 
