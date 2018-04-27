55b7505231d1c36435211fb14e7cf65315e04c4c
Nathaniel Green <ngreen2303@gmail.com>
2018-04-27 17:53:37 +0100

    BU-33: Added Navbar Icon and Link

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -8,11 +8,15 @@
       <img class="navbar__item-img" src="../static/icons/clipboard.svg" />
       <label>Daily Log</label>
     </div>
+    <div class="navbar__item" @click="goToMonthlyLog($router)">
+      <img class="navbar__item-img" src="../static/icons/calendar.svg" />
+      <label>Monthly Log</label>
+    </div>
     <nuxt-link class="navbar__item" to="/add-new">
       <img class="navbar__item-img" src="../static/icons/file-plus.svg" />
       <label>New Page</label>
     </nuxt-link>
-    <nuxt-link class="navbar__item" to="/menu">
+    <nuxt-link class="navbar__item navbar__item--menu" to="/menu">
       <img class="navbar__item-img" src="../static/icons/more-vertical.svg" />
       <label>More</label>
     </nuxt-link>
@@ -51,10 +55,15 @@ export default {
     text-align: center;
     margin: 0 auto;
     outline: none;
-    font-size: 14px;
+    font-size: 12px;
     word-wrap: break-word;
 
-    &:hover {
+    &--menu {
+      color: #ffffff;
+      flex: .5;
+    };
+
+    &:active {
       background-color: #f1f1f1;
     }
   }
