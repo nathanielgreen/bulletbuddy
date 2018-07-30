ee8272e893d3f9d08b9133e345ba8a90a0e1a0f9
Nathaniel Green <ngreen2303@gmail.com>
2018-07-30 18:11:02 +0100

    BB-35: Intialised Monthly Log component

---
+++ components/MonthlyLog.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="monthly-log">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MonthlyLog',
+};
+</script>
+
+<style lang="scss" scoped>
+.monthly-log {
+}
+</style>
