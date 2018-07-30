3d9fd0f1c2d5d1e2afbb13ecc9e39ab54f17a9b4
Nathaniel Green <ngreen2303@gmail.com>
2018-07-30 19:38:11 +0100

    BB-35: Added MonthlyLog component to page

--- store/modules/logTypeMonthly.js
+++ store/modules/logTypeMonthly.js
@@ -1,13 +1,20 @@
 import Moment from 'moment';
 
 const state = {
-  calendar: [],
+  logItems: [],
 };
 
 const getters = {
+  getLogItems: state => state.logItems,
 };
 
 const mutations = {
+  CLEAR_LOG_ITEMS(state) {
+    state.logItems = [];
+  },
+  SET_LOG_ITEMS(state, val) {
+    state.logItems = val;
+  },
 };
 
 const actions = {
@@ -32,11 +39,16 @@ const actions = {
       type: 'ML',
       header: Moment().format('MMMM YYYY'),
       content: {
-        calendar: calendarArr,
+        items: calendarArr,
       },
     };
 
     context.commit('CREATE_NEW_PAGE', newPage, { root: true });
+  },
+  setLogItems(context) {
+    const content = context.rootGetters['page/getViewedPageContent'];
+    const logItems = content.items;
+    if (logItems) { context.commit('SET_LOG_ITEMS', logItems); }
   },
 };
 
