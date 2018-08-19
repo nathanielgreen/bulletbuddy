642be75c1f08468a989b788b5ddbf11f972d854a
Nathaniel Green <ngreen2303@gmail.com>
2018-08-19 17:14:21 +0100

    BB-50: Added button to monthly log item

--- components/LogTypeMonthly.vue
+++ components/LogTypeMonthly.vue
@@ -1,11 +1,21 @@
 <template>
-  <div class="log--monthly">
+  <div class="monthly-log">
     <div
-      class="log--monthly__item"
+      class="monthly-log__item"
       v-for="item in items"
       v-bind:key="item.index"
     >
-      <span>{{ item.dayOfMonth }} {{ item.weekday }}</span>
+      <div class="monthly-log__item-date">
+        <span>{{ item.dayOfMonth }} {{ item.weekday }}</span>
+      </div>
+      <div class="monthly-log__item-options">
+        <button class="monthly-log__item-options-button">
+          <img
+            class="monthly-log__item-options-button-img"
+            src="../static/icons/plus.svg"
+          />
+        </button>
+      </div>
     </div>
   </div>
 </template>
@@ -36,11 +46,36 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.log--monthly {
+.monthly-log {
 
   &__item {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
     padding: 16px;
     font-size: 18px;
+
+    &-options {
+      &-button {
+        border: none;
+        border-radius: 6px;
+        background: #eeeeee;
+        width: 30px;
+        height: 30px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        &:hover {
+        }
+
+        &-img {
+          height: 20px;
+          width: 20px;
+        }
+      }
+
+    }
   }
 }
 </style>
